b96456355a0ad4d79894a0ba70d46063e9fa2451
NellyNell <129366078+TekNellCo@users.noreply.github.com>
2023-10-08 04:54:10 -0400

    finish js closes other tabs when one tab is open

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 document.addEventListener("click", (e) => {
   const dropDown = e.target.matches(".dropdown");
+  const question = e.target.matches(".question");
+  let dropDownAll = document.querySelectorAll(".dropdown");
 
-  if (dropDown) {
-    e.target.classList.toggle("active");
+  let currentDropdown;
+
+  if (dropDown || question) {
+    let currentDropdown = e.target.closest(".dropdown");
+    currentDropdown.classList.toggle("active");
+
+    dropDownAll.forEach((drop) => {
+      if (currentDropdown !== drop) {
+        drop.classList.remove("active");
+      }
+    });
   } else console.log("doesnt work");
 });
