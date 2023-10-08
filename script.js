639a6983480cafc8208a9c0c9b29fc99b22b4b10
NellyNell <129366078+TekNellCo@users.noreply.github.com>
2023-10-08 17:34:20 -0400

    change from let to const

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("click", (e) => {
   const dropDown = e.target.matches(".dropdown");
   const question = e.target.matches(".question");
-  let dropDownAll = document.querySelectorAll(".dropdown");
+  const dropDownAll = document.querySelectorAll(".dropdown");
 
   let currentDropdown;
 
   if (dropDown || question) {
-    let currentDropdown = e.target.closest(".dropdown");
+    currentDropdown = e.target.closest(".dropdown");
     currentDropdown.classList.toggle("active");
 
     dropDownAll.forEach((drop) => {
